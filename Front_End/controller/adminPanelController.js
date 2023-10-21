75cce0fe3b8738f40ffe739c5f9d2689a511d7ee
achinthajayashan <achintha.uns.2018@gmail.com>
2023-10-21 23:15:35 +0530

    car Add form

--- Front_End/controller/adminPanelController.js
+++ Front_End/controller/adminPanelController.js
@@ -1,3 +1,32 @@
-$('#btnSignOut').click(function () {
-    window.history.back();
+$('#customersSec').hide();
+$('#dashboardSec').show();
+$('#carsSec').hide();
+
+// $('#btnSignOut').click(function () {
+//     window.history.back();
+// });
+
+$('#customers-tab').click(function (e) {
+    console.log("clicked");
+    $("#dashboardSec").toggle();
+    $('#customersSec').toggle();
+    $('#carsSec').hide();
+    // $('#customersSec').show();
 });
+
+$('#dashboard-tab').click(function (e) {
+    console.log("clicked");
+    $("#dashboardSec").show();
+    $('#customersSec').hide();
+    $('#carsSec').hide();
+    // $('#customersSec').show();
+});
+
+$('#cars-tab').click(function (e) {
+    console.log("clicked");
+    $("#dashboardSec").hide();
+    $('#customersSec').hide();
+    $('#carsSec').show();
+    // $('#customersSec').show();
+});
+
