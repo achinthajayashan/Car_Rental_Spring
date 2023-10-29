9593d9b001a12afbfa9d21a1fc58fe8ad44aad87
achinthajayashan <achintha.uns.2018@gmail.com>
2023-10-29 22:19:25 +0530

    car On site view created

--- Front_End/controller/homePageController.js
+++ Front_End/controller/homePageController.js
@@ -11,3 +11,26 @@ $('#closeLogin').click(function () {
 $('#btnSign').click(function () {
     window.open("../pages/adminPanel.html","_self");
 });
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    if (index < 0) {
+        index = slides.length - 1;
+    } else if (index >= slides.length) {
+        index = 0;
+    }
+
+    slides[currentSlide].style.display = 'none';
+    slides[index].style.display = 'block';
+
+    currentSlide = index;
+}
+
+function changeSlide(offset) {
+    showSlide(currentSlide + offset);
+}
+
+// Initial display of the first slide
+showSlide(currentSlide);
