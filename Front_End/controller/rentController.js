91ed03a7dccbd139e30fc6aa79f1ebb050e9e9a2
achinthajayashan <achintha.uns.2018@gmail.com>
2023-11-03 17:22:38 +0530

    rental detail popup finished

--- Front_End/controller/rentController.js
+++ Front_End/controller/rentController.js
@@ -1,4 +1,7 @@
 getAllRents();
+
+
+$('#rentalDetailsPopupBg').hide();
 function getAllRents() {
     $("#tblRentRequests").empty();
     $.ajax({
@@ -8,6 +11,9 @@ function getAllRents() {
         // enctype:"multipart/form-data",
         // contentType: false,
         success: function (res) {
+            let resData = res.data;
+
+
             console.log(res);
 
             for (let i of res.data) {
@@ -22,11 +28,73 @@ function getAllRents() {
                 let row = "<tr><td>" + rentId + "</td><td>" + cusId + "</td><td>" + pickupDate + "</td><td>" + pickupTime + "</td><td>" + duration + "</td><td>" + status + "</td></tr>";
                 $("#tblRentRequests").append(row);
 
+                rentReqTableBindEvents();
+
                 // console.log(forntImage);
             }
+            function rentReqTableBindEvents() {
+                $('#tblRentRequests>tr').click(function () {
+                    $('#rentalDetailsPopupBg').show();
+                    $('#tblPopupRent').empty();
+                    let clickedRentId = $(this).children().eq(0).text();
+                    let clickedCusId = $(this).children().eq(1).text();
+                    let clickedPickupDate = $(this).children().eq(2).text();
+                    let clickedPickupTime = $(this).children().eq(3).text();
+                    let clickedDuration = $(this).children().eq(4).text();
+                    let currentStatus = $(this).children().eq(5).text();
+
+
+                    $('#lblRentId').text(clickedRentId);
+                    $('#lblCusId').text(clickedCusId);
+                    $('#lblPickupDate').text(clickedPickupDate);
+                    $('#lblPickupTime').text(clickedPickupTime);
+                    $('#lblDuration').text(clickedDuration);
+                    $('#lblCurrentStatus').text(currentStatus);
+
+
+                    // $('#rentIdtoHide').text(clickedRentId);
+
+                    for(let k = 0; k < resData.length; k++){
+
+                        if(resData[k].rentID == clickedRentId){
+
+                            for(let u = 0; u < resData[k].rentDetails.length; u++){
+
+                                let carID = resData[k].rentDetails[u].carID;
+                                let paymentID = resData[k].rentDetails[u].payment.paymentID;
+                                let wavier = resData[k].rentDetails[u].payment.waiverSlip;;
+                                let driverID = resData[k].rentDetails[u].driverID
+
+                                let row =`<tr><td>${carID}</td><td>${driverID}</td><td>${paymentID}</td><td><img src=' "+"/"+wavier+" ' width='100px'></td></tr>`;
+                                $('#tblPopupRent').append(row);
+
+                            }
+
+                            break;
+                        }
+
+                    }
+
+                });
+            }
+
             console.log(res.message);
         }, error: function (error) {
             // alert(error.responseJSON.message);
         }
     });
 }
+function rentReqTableBindEvents() {
+    $('#tblRentRequests>tr').click(function () {
+        $('#rentalDetailsPopupBg').show();
+
+
+    })
+}
+
+
+
+$('#btnRentalPopupClose').click(function () {
+    $('#rentalDetailsPopupBg').hide();
+
+})
